66b048e26524f365c45d028840a748bf1de8ff7c
LoSt <zhengyuhang47@qq.com>
2018-09-20 10:55:57 +0800

    优化模板项目文件; 完全分离开发 / 生产环境文件位置;
    增加css js版本号控制,图片压缩优化;

--- config/gulp.build.config.js
+++ config/gulp.build.config.js
@@ -3,34 +3,53 @@ const gulp = require('gulp')
 const stylus = require('gulp-stylus')
 const cssmin = require('gulp-clean-css')
 const autoprefixer = require('gulp-autoprefixer')
-const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
-const base64 = require('gulp-base64')
+const imagemin = require('gulp-imagemin')
 const del = require('del')
 const gulpSequence = require('gulp-sequence')
+const htmlmin = require('gulp-htmlmin')
+const rev = require('gulp-revm')
+const revCollector = require('gulp-revm-collector')
 
+let destImages = 'dist/imgs'
 let destStylus = 'dist/styles'
 let destScripts = 'dist/scripts'
 
+let revCss = 'rev/css'
+let revJs = 'rev/js'
+
 function production() {
   // 生产环境
+  gulp.task('imgOptimize', function () {
+    gulp.src([
+      'dev/imgs/*',
+      'dev/imgs/**/*'
+    ])
+      .pipe(imagemin({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true,
+        multipass: true
+      }))
+      .pipe(gulp.dest(destImages)
+      )
+  })
   gulp.task('styles', function () {
     return gulp.src('dev/stylus/*.styl')
-      .pipe(base64({
-        baseDir: 'dist/images',
-        extensions: ['svg', 'png', /\.jpg#datauri$/i],
-        exclude: [/\.server\.(com|net)\/dynamic\//, '--live.jpg'],
-        maxImageSize: 10 * 1024,
-        debug: true
-      }))
       .pipe(stylus())
       .pipe(autoprefixer())
       .pipe(cssmin({
         keepSpecialComments: '*'
       }))
-      .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest(destStylus))
+  })
+  gulp.task('revStyles', function () {
+    return gulp.src('dist/styles/*.css')
+      .pipe(rev())
+      .pipe(gulp.dest(destStylus))
+      .pipe(rev.manifest())
+      .pipe(gulp.dest(revCss))
   })
   gulp.task('scripts', function () {
     gulp.src([
@@ -40,18 +59,36 @@ function production() {
       .pipe(babel({
         presets: ['env']
       }))
-      .pipe(rename({suffix: '.min'}))
       .pipe(uglify())
+      .pipe(rev())
       .pipe(gulp.dest(destScripts))
+      .pipe(rev.manifest())
+      .pipe(gulp.dest(revJs))
+  })
+  gulp.task('rev', function () {
+    return gulp.src(['rev/**/*.json', 'dev/*.html', 'dev/pages/*.html'])
+      .pipe(revCollector({
+        replaceReved: true,
+        dirReplacements: {
+          'styles': 'styles',
+          'scripts': 'scripts',
+          'imgs': 'imgs'
+        }
+      }))
+      .pipe(htmlmin({
+        empty: true,
+        spare: true
+      }))
+      .pipe(gulp.dest('dist'))
   })
 
   // 清理开发环境文件
   gulp.task('clean', function () {
-    return del([destStylus, destScripts])
+    return del('dist')
   })
 
   // 生成生产环境文件
-  gulp.task('build', gulpSequence('clean', 'styles', 'scripts'))
+  gulp.task('build', gulpSequence('clean', ['styles', 'scripts', 'imgOptimize'], 'revStyles', 'rev'))
 }
 
 module.exports = production
--- config/gulp.dev.config.js
+++ config/gulp.dev.config.js
@@ -2,12 +2,14 @@
 const gulp = require('gulp')
 const stylus = require('gulp-stylus')
 const sourcemaps = require('gulp-sourcemaps')
-const rename = require('gulp-rename')
 const browserSync = require('browser-sync')
 const reload = browserSync.reload
 const changed = require('gulp-changed')
 const gulpSequence = require('gulp-sequence')
+const htmlmin = require('gulp-htmlmin')
+const imagemin = require('gulp-imagemin')
 
+let destImages = 'dist/imgs'
 let destStylus = 'dist/styles'
 let destScripts = 'dist/scripts'
 
@@ -16,7 +18,7 @@ function development() {
   gulp.task('serve', function () {
     browserSync({
       server: {
-        baseDir: './'
+        baseDir: './dist'
       }
     })
     gulp.watch(['dist/*', 'dist/**/*', 'dist/**/**/*'], {cwd: './'}, reload)
@@ -25,15 +27,33 @@ function development() {
   gulp.task('watch', function () {
     gulp.watch(['dev/stylus/*', 'dev/stylus/**/*'], ['devStyles'])
     gulp.watch(['dev/js/*', 'dev/js/**/*'], ['devScripts'])
+    gulp.watch(['dev/*.html', 'dev/pages/*.html'], ['htmlMin'])
+    gulp.watch(['dev/imgs/*', 'dev/imgs/**/*'], ['imgOptimize'])
   })
 
   // 开发环境
+  gulp.task('imgOptimize', function () {
+    gulp.src([
+      'dev/imgs/*',
+      'dev/imgs/**/*'
+    ])
+      .pipe(changed(destImages))
+      .pipe(imagemin({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true,
+        multipass: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [pngquant()]
+      }))
+      .pipe(gulp.dest(destImages)
+      )
+  })
   gulp.task('devStyles', function () {
     return gulp.src('dev/stylus/*.styl')
       .pipe(changed(destStylus))
       .pipe(sourcemaps.init())
       .pipe(stylus())
-      .pipe(rename({suffix: '.min'}))
       .pipe(sourcemaps.write("maps"))
       .pipe(gulp.dest(destStylus))
   })
@@ -44,11 +64,18 @@ function development() {
     ])
       .pipe(changed(destScripts))
       .pipe(sourcemaps.init())
-      .pipe(rename({suffix: '.min'}))
       .pipe(sourcemaps.write('maps'))
       .pipe(gulp.dest(destScripts))
   })
-  gulp.task('dev', gulpSequence(['devStyles', 'devScripts'], 'serve', 'watch'))
+  gulp.task('htmlMin', function () {
+    return gulp.src([
+      'dev/*.html',
+      'dev/pages/*.html'
+    ])
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('dist'));
+  })
+  gulp.task('dev', gulpSequence(['devStyles', 'devScripts', 'htmlMin', 'imgOptimize'], 'serve', 'watch'))
 }
 
 module.exports = development
